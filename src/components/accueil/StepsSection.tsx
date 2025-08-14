d8bfec094ad0a9ee856843a9c5be9b75f00f1d82
Félix-Antoine Leduc <felixantoineleduc@gmail.com>
2025-08-13 22:28:58 -0400

    Merge des components des steps

--- src/components/accueil/StepsSection.tsx
+++ src/components/accueil/StepsSection.tsx
@@ -1,10 +1,33 @@
-import ThreeSteps from "@/components/accueil/ThreeSteps";
-
 export default function StepsSection() {
   return (
     <div className="grid grid-cols-3 gap-16 pb-28">
-      <ThreeSteps />
+      <div className="flex items-center gap-6">
+        <p className="font-finalsix text-6xl font-[900] text-white rounded-full px-[20px] py-2 bg-[#1E1F3F]">
+          1
+        </p>
+        <p>
+          Coaching ancré dans la <br />
+          réalité organisationnelle
+        </p>
+      </div>
+      <div className="flex items-center gap-6">
+        <p className="font-finalsix text-6xl font-[900] text-white rounded-full px-[17px] py-2 bg-[#1E1F3F]">
+          2
+        </p>
+        <p>
+          Expérience issue du <br />
+          terrain mise à votre profit
+        </p>
+      </div>
+      <div className="flex items-center gap-6">
+        <p className="font-finalsix text-6xl font-[900] text-white rounded-full px-[18px] py-2 bg-[#1E1F3F]">
+          3
+        </p>
+        <p>
+          Souplesse pour accompagner <br />
+          les gestionnaires intermédiaires
+        </p>
+      </div>
     </div>
   );
 }
-
