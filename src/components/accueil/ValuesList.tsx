86188fc01f7c2d293cd2d215405c3fff2c11f1b9
felixleduc <97909315+felixleduc@users.noreply.github.com>
2025-08-27 12:37:45 -0400

    refactor: make values list wrap naturally

--- src/components/accueil/ValuesList.tsx
+++ src/components/accueil/ValuesList.tsx
@@ -13,12 +13,14 @@ export default function ValuesList() {
   return (
     <section className="px-2">
       <h4 className="font-finalsix text-3xl font-bold mb-4 md:hidden">VALEURS</h4>
-      <div className="grid grid-cols-2 gap-2 md:grid-cols-3 md:gap-2">
-        <h4 className="hidden md:block font-finalsix text-3xl font-bold">VALEURS</h4>
+      <div className="flex flex-wrap gap-2">
+        <h4 className="hidden md:block font-finalsix text-3xl font-bold whitespace-nowrap">
+          VALEURS
+        </h4>
         {values.map((label) => (
           <p
             key={label}
-            className="text-xl border-[1px] border-[#1E1F3F] rounded-full px-6 py-2 hover:scale-105"
+            className="text-xl border border-[#1E1F3F] rounded-full px-6 py-2 w-fit whitespace-nowrap"
           >
             {label}
           </p>
